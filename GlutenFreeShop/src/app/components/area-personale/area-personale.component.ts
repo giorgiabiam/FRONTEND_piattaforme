8557b6d7a9ff0f02d44fe1ade56504bafd6bbf6d
giorgiabiam <biamontegiorgia@gmail.com>
2023-05-28 12:05:46 +0200

    soluzione signin arrangiata

--- GlutenFreeShop/src/app/components/area-personale/area-personale.component.ts
+++ GlutenFreeShop/src/app/components/area-personale/area-personale.component.ts
@@ -22,6 +22,7 @@ export class AreaPersonaleComponent {
   hide: boolean= true;
   loggato : boolean = false;   //questo lo dovrei mandare al parent
   dialog : boolean = false
+  messaggio_signin = false
 
   sign_username:String='';
   sign_password:String='';
@@ -31,14 +32,15 @@ export class AreaPersonaleComponent {
   signin_ok: boolean = false;
 
   password_control = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  username_control = new FormControl('', [Validators.required]);
 
   constructor(private user_service: UserService, private home_service: HomeService, private router:Router) {
     this.acquisti = []
   }
 
   ngOnInit() {
-    // console.log("area-personale -> utente:", sessionStorage.getItem("user_id"))
-    // console.log("area-personale -> token:", sessionStorage.getItem("token"))
+    console.log("area-personale -> utente:", sessionStorage.getItem("user_id"))
+    console.log("area-personale -> token:", sessionStorage.getItem("token"))
 
     if(sessionStorage.getItem("user_id")==null){
       this.loggato = false
@@ -64,6 +66,9 @@ export class AreaPersonaleComponent {
         sessionStorage.setItem("user_id", idutente);
         sessionStorage.setItem("token", jwt);
 
+
+        this.messaggio_signin = false
+
         this.getUser();
       },
 
@@ -84,22 +89,24 @@ export class AreaPersonaleComponent {
       console.log("GET USER", data)
       this.utente = JSON.parse(JSON.stringify(data));
 
-      this.user_service.getAcquisti(id).subscribe(data=>{
-        console.log("acquisti dell'utente ",id, ": ", data)
-        let lista:Acquisto[] = []
-
-        //TODO controlla cosa arriva dal server
-
-        for(let a of JSON.parse(JSON.stringify(data))){
-          console.log("A: ", a)
-          let acquisto:Acquisto = new Acquisto(a.dataAcquisto, a.listaProdotti, a.tot, a.id, this.utente)
-          lista.push(acquisto)
-        }
-        this.acquisti = lista
-        console.log("this acquisti ", this.acquisti)
-      });
+    //  this.getAcquisti(id);
 
     })
+  }
+
+  getAcquisti(id:string|null){
+    this.user_service.getAcquisti(id).subscribe(data=>{
+      console.log("acquisti dell'utente ",id, ": ", data)   //TODO non stampa nulla
+      let lista:Acquisto[] = []
+
+      for(let a of JSON.parse(JSON.stringify(data))){
+        console.log("A: ", a)
+        let acquisto:Acquisto = new Acquisto(a.dataAcquisto, a.listaProdotti, a.tot, a.id, this.utente)
+        lista.push(acquisto)
+      }
+      this.acquisti = lista
+      console.log("this acquisti ", this.acquisti)
+    });
   }
 
   signin(){
@@ -108,22 +115,22 @@ export class AreaPersonaleComponent {
 
     this.user_service.signin(newUtente).subscribe({
       next:data=>{
-        console.log("SIGNIN----", data)  // restituisce un JwtResponse formato da id_utente e jwt
         this.signin_ok = true;
-        this.loggato = true;
+        // this.loggato = true;
 
         let response = JSON.parse(JSON.stringify(data));
-        const jwt = response.token
-        const idutente = response.idutente
+        console.log("SIGNIN----", response)  // TODO restituisce solo l'utente ma deve restituire anche il jwt !!!!
 
-        sessionStorage.setItem("user_id", idutente);
-        sessionStorage.setItem("token", jwt);
+        // sessionStorage.setItem("user_id", response.id)
+        // console.log("in signin user id",sessionStorage.getItem("user_id"))
 
-        this.getUser()
+        // this.getUser()
+        this.nuovoUtente = false
+        this.messaggio_signin = true
       },
       error:error=>{
         this.signin_ok = false;
-        this.loggato = false;
+        // this.loggato = false;
         console.log("errore signin", error)
       }
 
--- GlutenFreeShop/src/app/components/carrello/carrello.component.ts
+++ GlutenFreeShop/src/app/components/carrello/carrello.component.ts
@@ -6,6 +6,7 @@ import { ItemCarrello } from 'src/app/models/ItemCarrello';
 
 import { HomeService } from 'src/app/services/home-service.service';
 import { UserService } from 'src/app/services/user-service.service';
+import { Utente } from 'src/app/models/Utente';
 
 @Component({
   selector: 'app-carrello',
@@ -17,6 +18,7 @@ export class CarrelloComponent implements OnInit {
   carrello_vuoto:boolean = true;
 
   ok_acquisto!:any
+  indirizzo!:String
 
   constructor(private user_service : UserService, private home_service : HomeService, private router:Router){ }
 
@@ -49,6 +51,8 @@ export class CarrelloComponent implements OnInit {
     this.home_service.removeFromCart(p.codice).subscribe({
       next:data=>{
         console.log("rimozione prodotto", p, data)
+        // this.carrello = JSON.parse(JSON.stringify(data))
+
         this.loadCarrello()
       },
       error:err=>{
@@ -73,6 +77,7 @@ export class CarrelloComponent implements OnInit {
       next:data=>{
         console.log("dati acquisto andato a buon fine ", data)
         this.ok_acquisto = true
+        this.getIndirizzo()
         this.home_service.svuota_carrello().subscribe( data=>{
           console.log("carrello svuotato dopo l'acquisto", data);
           this.carrello_vuoto = true
@@ -95,12 +100,27 @@ export class CarrelloComponent implements OnInit {
 
   }
 
-  aggiorna_quantita(itemCArrello:ItemCarrello){
-     this.home_service.addToCart(itemCArrello.prodotto.codice, itemCArrello.qta_acquist).subscribe(data=>{
-      debugger;
+  aggiorna_quantita(itemCarrello:ItemCarrello){
+    if(itemCarrello.qta_acquist == 0){
+      //TODO cazzo cazzo
+    }
+
+    this.home_service.addToCart(itemCarrello.prodotto.codice, itemCarrello.qta_acquist).subscribe(data=>{
        console.log("DATA", data)
        this.carrello = JSON.parse(JSON.stringify(data))
-     })
+    })
 
+  }
+
+  getIndirizzo(){
+    let id = sessionStorage.getItem("user_id")
+
+    this.user_service.getById(id).subscribe(data=>{
+      console.log("GET INDIRIZZO", data)
+      let utente:Utente = JSON.parse(JSON.stringify(data));
+
+      this.indirizzo =  utente.indirizzo
+
+    })
   }
 }
